9f63a671b5d773816ef84949e0c787b2fd5717d5
AutomationWithPiyushhh <vasudevahir21@gmail.com>
2026-08-27 14:43:53 +0530

    refactor: centralize theme management into a dedicated script and persist user preference via localStorage

---
+++ js/theme.js
@@ -0,0 +1,18 @@
+(function () {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+})();
+
+function toggleTheme() {
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    }
+}
